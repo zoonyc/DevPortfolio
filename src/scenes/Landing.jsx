55faabed1019a4c064fe08b13a1ba751f34ffb0c
zoonyc <zoonyc_117@hotmail.com>
2025-11-03 15:31:23 -0600

    Actualización de Landing

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -11,27 +11,31 @@ const Landing = ({ setSelectedPage }) => {
       id="home"
       className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
     >
-      {/* IMAGE SECTION */}
-      <div className="basis-3/5 z-10 mt-16 md:mt-20 flex justify-center md:order-2">
-        {isAboveLarge ? (
-          <div
-            className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 before:rounded-t-[400px]
-            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
-          >
-            <img
-              alt="profile"
-              className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-              src="assets/profile-image.png"
-            />
-          </div>
-        ) : (
-          <img
-            alt="profile"
-            className="z-10 w-full max-w-[400px] md:max-w-[600px]"
-            src="assets/profile-image.png"
-          />
-        )}
-      </div>
+
+
+     {/* IMAGE SECTION */}
+<div className="basis-3/5 z-10 mt-16 md:mt-20 flex justify-center md:order-2">
+  {isAboveLarge ? (
+    <div
+      className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 
+      before:rounded-t-[400px] before:w-full before:max-w-[400px] md:before:max-w-[600px] 
+      before:h-full before:border-2 before:border-blue before:z-[-1]"
+    >
+      <img
+        alt="profile"
+        src="assets/Profile_image.webp"
+        className="hover:filter hover:saturate-150 transition duration-500 
+        z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px]"
+      />
+    </div>
+  ) : (
+    <img
+      alt="profile"
+      src="assets/Profile_image.webp"
+      className="z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px]"
+    />
+  )}
+</div>
 
       {/* MAIN TEXT */}
       <div className="z-30 basis-2/5 mt-12 md:mt-32">
